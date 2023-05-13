5060127f2d6fb37f5adba47137fd63fc7483d0d8
unknown <mahmudul.cse.eng@gmail.com>
2023-05-13 23:36:27 +0600

    dropdown add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,10 +10,16 @@ function Home() {
     
           <div className='header-area container'>
             <div className='endbg'></div>
-          <ul>
+          <ul className='header-main-ul'>
             <li className='header-logo'> <img src={logo} alt="#"/> </li>
             <li><Link to="../home">Home</Link></li>
-            <li><Link to="../fruits">Fruits</Link></li>
+            <li className='dropdown'><ul className='drop-menu'>Fruits
+                  <li className='drop-item'>Mango</li>
+                  <li className='drop-item'>Banana</li>
+                  <li className='drop-item'>Jam</li>
+                  <li className='drop-item'>Lichu</li>
+            </ul>
+            </li>
             <li><Link to="../about">About Us</Link></li>
             <li><Link to="../login">Login</Link></li>
           </ul>
